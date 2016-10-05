a656dc349817749011ece7b696aa5c9e8e19ec5f
Jhalak K Gurung <jkgurung@berkeley.edu>
2016-10-05 16:50:54 -0700

    Calculator Implemented

--- SwiftCalc/SwiftCalc/ViewController.swift
+++ SwiftCalc/SwiftCalc/ViewController.swift
@@ -13,17 +13,115 @@ class ViewController: UIViewController {
     var w: CGFloat!
     var h: CGFloat!
     
-
+    
     // IMPORTANT: Do NOT modify the name or class of resultLabel.
     //            We will be using the result label to run autograded tests.
     // MARK: The label to display our calculations
     var resultLabel = UILabel()
     
+    
     // TODO: This looks like a good place to add some data structures.
     //       One data structure is initialized below for reference.
-    var someDataStructure: [String] = [""]
+    var someDataStructure: [String] = []
+    //added
+    var userMiddleOfTyping = false
+    var accumulator = 0.0
     
-
+    private var pending: pendingBinaryOperationInfo?
+    //stores the firstOperand and operator
+    struct pendingBinaryOperationInfo {
+        var binaryFunction: (Double, Double)-> Double
+        var firstOperand: Double
+    }
+    
+    var result: Double {
+        get {
+            return accumulator
+        }
+    }
+    
+    //added
+    private var displayValueDouble: Double {
+        get {
+            return Double(resultLabel.text!)!
+        }
+        set {
+            resultLabel.text = String(newValue)
+        }
+    }
+    
+    private var displayValueInt: Int {
+        get {
+            return Int(resultLabel.text!)!
+        }
+        set {
+            resultLabel.text = String(newValue)
+        }
+    }
+    
+    func checkSevenCharLong() -> Bool {
+        let str = resultLabel.text!
+        return str.characters.count < 7
+        
+    }
+    
+    //    func makeSevenCharLong() {
+    //        let str = String(result)
+    //        if str.characters.count > 7 {
+    //            let h = str.characters.dropLast(str.characters.count - 7)
+    //
+    //        }
+    //
+    //    }
+    
+    
+    
+    
+    func setOperand(operand: Double) {
+        accumulator = operand
+    }
+    
+    var operations: Dictionary<String, operation> = [
+        "+/-": operation.UnaryOperation({-$0}),
+        "/" : operation.BinaryOperation({$0 / $1}),
+        "+" : operation.BinaryOperation({$0 + $1}),
+        "-" : operation.BinaryOperation({$0 - $1}),
+        "*" : operation.BinaryOperation({$0 * $1}),
+        "=" : operation.Equals,
+        "C" : operation.Clear
+    ]
+    
+    enum operation {
+        case Clear
+        case UnaryOperation((Double)-> Double)
+        case BinaryOperation((Double, Double)-> Double)
+        case Equals
+    }
+    
+    func performOperation(symbol: String) {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .UnaryOperation(let function):
+                accumulator = function(accumulator)
+            case .BinaryOperation(let function):
+                executePendingBinaryOperation()
+                pending = pendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
+            case .Equals:
+                executePendingBinaryOperation()
+            case .Clear:
+                clean()
+                
+            }
+        }
+    }
+    
+    func executePendingBinaryOperation() {
+        if pending != nil {
+            accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+            pending = nil
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view = UIView(frame: UIScreen.main.bounds)
@@ -38,6 +136,53 @@ class ViewController: UIViewController {
         // Do any additional setup here.
     }
     
+    // TODO: Ensure that resultLabel gets updated.
+    //       Modify this one or create your own.
+    func updateResultLabel(_ content: String) {
+        if checkSevenCharLong() {
+            let digit = content
+            if userMiddleOfTyping {
+                let textCurrentlyDisplay = resultLabel.text!
+                resultLabel.text = textCurrentlyDisplay + digit
+            } else {
+                resultLabel.text = digit
+            }
+            userMiddleOfTyping = true
+        }
+    }
+    
+    // REQUIRED: The responder to a number button being pressed.
+    func numberPressed(_ sender: CustomButton) {
+        guard Int(sender.content) != nil else { return }
+        updateResultLabel(sender.content)
+    }
+    
+    // REQUIRED: The responder to an operator button being pressed.
+    func operatorPressed(_ sender: CustomButton) {
+        if userMiddleOfTyping {
+            setOperand(operand: displayValueDouble)
+            userMiddleOfTyping = false
+        }
+        performOperation(symbol: sender.content)
+        let checkInt = Int(result)
+        if result - Double(checkInt) == 0 {
+            displayValueInt = checkInt
+        } else {
+            displayValueDouble = result
+        }
+        
+    }
+    
+    // REQUIRED: The responder to a number or operator button being pressed.
+    func buttonPressed(_ sender: CustomButton) {
+        updateResultLabel(sender.content)
+    }
+    func clean() {
+        userMiddleOfTyping = false
+        accumulator = 0.0
+        pending = nil
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -46,51 +191,25 @@ class ViewController: UIViewController {
     // TODO: A method to update your data structure(s) would be nice.
     //       Modify this one or create your own.
     func updateSomeDataStructure(_ content: String) {
-        print("Update me like one of those PCs")
+        print("Update me like one of those PCs updateSomeData")
     }
-    
-    // TODO: Ensure that resultLabel gets updated.
-    //       Modify this one or create your own.
-    func updateResultLabel(_ content: String) {
-        print("Update me like one of those PCs")
-    }
-    
     
     // TODO: A calculate method with no parameters, scary!
     //       Modify this one or create your own.
     func calculate() -> String {
-        return "0"
+        return ""
     }
     
     // TODO: A simple calculate method for integers.
     //       Modify this one or create your own.
     func intCalculate(a: Int, b:Int, operation: String) -> Int {
-        print("Calculation requested for \(a) \(operation) \(b)")
         return 0
     }
     
     // TODO: A general calculate method for doubles
     //       Modify this one or create your own.
     func calculate(a: String, b:String, operation: String) -> Double {
-        print("Calculation requested for \(a) \(operation) \(b)")
         return 0.0
-    }
-    
-    // REQUIRED: The responder to a number button being pressed.
-    func numberPressed(_ sender: CustomButton) {
-        guard Int(sender.content) != nil else { return }
-        print("The number \(sender.content) was pressed")
-        // Fill me in!
-    }
-    
-    // REQUIRED: The responder to an operator button being pressed.
-    func operatorPressed(_ sender: CustomButton) {
-        // Fill me in!
-    }
-    
-    // REQUIRED: The responder to a number or operator button being pressed.
-    func buttonPressed(_ sender: CustomButton) {
-       // Fill me in!
     }
     
     // IMPORTANT: Do NOT change any of the code below.
@@ -123,7 +242,7 @@ class ViewController: UIViewController {
             guard let container = element as? UIView else { return }
             container.backgroundColor = UIColor.black
         }
-
+        
         let margin: CGFloat = 1.0
         let buttonWidth: CGFloat = w / 4.0
         let buttonHeight: CGFloat = 100.0
@@ -133,9 +252,9 @@ class ViewController: UIViewController {
             let x = (CGFloat(i%3) + 1.0) * margin + (CGFloat(i%3) * buttonWidth)
             let y = (CGFloat(i/3) + 1.0) * margin + (CGFloat(i/3) * buttonHeight)
             calcContainer.addUIElement(CustomButton(content: el), text: el,
-            frame: CGRect(x: x, y: y, width: buttonWidth, height: buttonHeight)) { element in
-                guard let button = element as? UIButton else { return }
-                button.addTarget(self, action: #selector(operatorPressed), for: .touchUpInside)
+                                       frame: CGRect(x: x, y: y, width: buttonWidth, height: buttonHeight)) { element in
+                                        guard let button = element as? UIButton else { return }
+                                        button.addTarget(self, action: #selector(operatorPressed), for: .touchUpInside)
             }
         }
         // MARK: Second Row 3x3
@@ -143,9 +262,9 @@ class ViewController: UIViewController {
             let x = (CGFloat(i%3) + 1.0) * margin + (CGFloat(i%3) * buttonWidth)
             let y = (CGFloat(i/3) + 1.0) * margin + (CGFloat(i/3) * buttonHeight)
             calcContainer.addUIElement(CustomButton(content: digit), text: digit,
-            frame: CGRect(x: x, y: y+101.0, width: buttonWidth, height: buttonHeight)) { element in
-                guard let button = element as? UIButton else { return }
-                button.addTarget(self, action: #selector(numberPressed), for: .touchUpInside)
+                                       frame: CGRect(x: x, y: y+101.0, width: buttonWidth, height: buttonHeight)) { element in
+                                        guard let button = element as? UIButton else { return }
+                                        button.addTarget(self, action: #selector(numberPressed), for: .touchUpInside)
             }
         }
         // MARK: Vertical Column of Operators
@@ -153,11 +272,11 @@ class ViewController: UIViewController {
             let x = (CGFloat(3) + 1.0) * margin + (CGFloat(3) * buttonWidth)
             let y = (CGFloat(i) + 1.0) * margin + (CGFloat(i) * buttonHeight)
             calcContainer.addUIElement(CustomButton(content: el), text: el,
-            frame: CGRect(x: x, y: y, width: buttonWidth, height: buttonHeight)) { element in
-                guard let button = element as? UIButton else { return }
-                button.backgroundColor = UIColor.orange
-                button.setTitleColor(UIColor.white, for: .normal)
-                button.addTarget(self, action: #selector(operatorPressed), for: .touchUpInside)
+                                       frame: CGRect(x: x, y: y, width: buttonWidth, height: buttonHeight)) { element in
+                                        guard let button = element as? UIButton else { return }
+                                        button.backgroundColor = UIColor.orange
+                                        button.setTitleColor(UIColor.white, for: .normal)
+                                        button.addTarget(self, action: #selector(operatorPressed), for: .touchUpInside)
             }
         }
         // MARK: Last Row for big 0 and .
@@ -165,12 +284,12 @@ class ViewController: UIViewController {
             let myWidth = buttonWidth * (CGFloat((i+1)%2) + 1.0) + margin * (CGFloat((i+1)%2))
             let x = (CGFloat(2*i) + 1.0) * margin + buttonWidth * (CGFloat(i*2))
             calcContainer.addUIElement(CustomButton(content: el), text: el,
-            frame: CGRect(x: x, y: 405, width: myWidth, height: buttonHeight)) { element in
-                guard let button = element as? UIButton else { return }
-                button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+                                       frame: CGRect(x: x, y: 405, width: myWidth, height: buttonHeight)) { element in
+                                        guard let button = element as? UIButton else { return }
+                                        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
             }
         }
     }
-
+    
 }
 
